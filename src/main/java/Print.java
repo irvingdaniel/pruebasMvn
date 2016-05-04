74d7f42ab861e9fd9454be76b0092d2d339dafac
edwincalzada <cal.edw.3@gmail.com>
2016-05-04 18:01:31 -0500

    cambios a la clase, se agragan parentesis para que sume los números y los separe de las cadenas

--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -1,10 +1,13 @@
-  public class Print {
-	  
-	  public static void main(final String[] args) {
-		  
-		  final String s = 2 + 2 + "ABC" + 1 + 2 + "DEF" + 3 + 4 + "FOUR" + "FIVE" + 5;
+public class Print {
 
-		  System.out.println(s);
-}
+	public static void main(final String[] args) {
+
+		final String s = 2 + 2 + "ABC" + (1 + 2) + (2 + 2) + "DEF" + 3 + 4
+				+ "FOUR" + "FIVE" + 5;
+
+		System.out.println(s);
+
+	}
+
 
 }
