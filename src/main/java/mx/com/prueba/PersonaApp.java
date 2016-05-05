400dfd0a54fc41528118643902a240a6625636a5
edwincalzada <cal.edw.3@gmail.com>
2016-05-05 09:41:56 -0500

    cambios del head

--- src/main/java/mx/com/prueba/PersonaApp.java
+++ src/main/java/mx/com/prueba/PersonaApp.java
@@ -7,14 +7,14 @@ public class PersonaApp {
     static Persona persona3;
     static Persona persona4;
     
-	public static void main(String[] args){
+	public static void main(final String[] args){
 	   persona1 = persona2;
 	   persona1= new Persona();
 	   persona3 = new Persona();
 	   persona3.comparaPersonaEquals(persona1, persona2);
 	   
-	   String nombre1="Juan";
-	   String nombre2="Pedro";
+	   final String nombre1="Juan";
+	   final String nombre2="Pedro";
 	   
 	   System.out.println(nombre1==nombre2);
 	   System.out.println(nombre1.equals(nombre2));
